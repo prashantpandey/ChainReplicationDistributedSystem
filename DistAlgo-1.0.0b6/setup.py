2f922362d80b6e5f7eb9b626064c580371d22e97
Prashant Pandey <prashant.prashn@gmail.com>
2014-10-07 18:45:34 -0400

    removing dist algo source from repo

--- DistAlgo-1.0.0b6/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-import da
-
-setup(name = "DistAlgo",
-      version = da.__version__,
-      author= "bolin",
-      author_email = "bolin@cs.stonybrook.edu",
-      packages = ['da', 'da.compiler'])
