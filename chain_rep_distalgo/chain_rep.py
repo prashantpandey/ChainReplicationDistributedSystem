7717fcf0594176fa4ad5f983739afecfa5065851
Prashant Pandey <prashant.prashn@gmail.com>
2014-10-09 04:21:44 -0400

    Added await condition. Not working properly.

--- chain_rep_distalgo/chain_rep.py
+++ chain_rep_distalgo/chain_rep.py
@@ -4,11 +4,8 @@ PatternExpr_0 = da.pat.TuplePattern([da.pat.ConstantPattern('Query'), da.pat.Fre
 PatternExpr_1 = da.pat.FreePattern('p')
 PatternExpr_2 = da.pat.TuplePattern([da.pat.ConstantPattern('Update'), da.pat.FreePattern('req')])
 PatternExpr_3 = da.pat.FreePattern('p')
-PatternExpr_4 = da.pat.TuplePattern([da.pat.ConstantPattern('StopServing')])
-PatternExpr_5 = da.pat.FreePattern('p')
-PatternExpr_6 = da.pat.TuplePattern([da.pat.ConstantPattern('Response')])
-PatternExpr_7 = da.pat.FreePattern('rclk')
-PatternExpr_9 = da.pat.TuplePattern([da.pat.ConstantPattern('Response'), da.pat.FreePattern('res')])
+PatternExpr_4 = da.pat.TuplePattern([da.pat.ConstantPattern('Response'), da.pat.FreePattern('res')])
+PatternExpr_6 = da.pat.TuplePattern([da.pat.ConstantPattern('Response'), da.pat.FreePattern('res')])
 import sys
 import json
 import pickle
@@ -32,25 +29,25 @@ class Server(da.DistProcess):
 
     def __init__(self, parent, initq, channel, props):
         super().__init__(parent, initq, channel, props)
-        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_0', PatternExpr_0, sources=[PatternExpr_1], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_0]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_1', PatternExpr_2, sources=[PatternExpr_3], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_1]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_2', PatternExpr_4, sources=[PatternExpr_5], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_2])])
+        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_0', PatternExpr_0, sources=[PatternExpr_1], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_0]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_1', PatternExpr_2, sources=[PatternExpr_3], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_1])])
 
     def main(self):
         _st_label_23 = 0
         while (_st_label_23 == 0):
             _st_label_23 += 1
-            if self.stopServing:
+            if (self.cnt == self.total_pings):
                 _st_label_23 += 1
             else:
                 super()._label('_st_label_23', block=True)
                 _st_label_23 -= 1
 
     def setup(self, total_pings, serverId):
-        self.serverId = serverId
         self.total_pings = total_pings
-        self.stopServing = False
+        self.serverId = serverId
+        self.cnt = 0
         self.accDetails = {}
 
-    def _Server_handler_0(self, req, p):
+    def _Server_handler_0(self, p, req):
         self.output(json.dumps(req))
         self.output(((('Received request: ' + str(req['reqId'])) + ' from client: ') + str(req['clientId'])))
         num = req['accNum']
@@ -67,7 +64,7 @@ class Server(da.DistProcess):
     _Server_handler_0._labels = None
     _Server_handler_0._notlabels = None
 
-    def _Server_handler_1(self, req, p):
+    def _Server_handler_1(self, p, req):
         self.output(json.dumps(req))
         self.output(((('Received request: ' + str(req['reqId'])) + ' from client: ') + str(req['clientId'])))
         num = req['accNum']
@@ -96,20 +93,12 @@ class Server(da.DistProcess):
     _Server_handler_1._labels = None
     _Server_handler_1._notlabels = None
 
-    def _Server_handler_2(self, p):
-        res = {}
-        res['outcome'] = 'Stopping'
-        self._send(('Response', res), p)
-        self.stopServing = True
-    _Server_handler_2._labels = None
-    _Server_handler_2._notlabels = None
-
 class Client(da.DistProcess):
 
     def __init__(self, parent, initq, channel, props):
         super().__init__(parent, initq, channel, props)
         self._ClientReceivedEvent_0 = []
-        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_0', PatternExpr_6, sources=None, destinations=None, timestamps=[PatternExpr_7], record_history=True, handlers=[]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_1', PatternExpr_9, sources=None, destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_3])])
+        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_0', PatternExpr_4, sources=None, destinations=None, timestamps=None, record_history=True, handlers=[]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_1', PatternExpr_6, sources=None, destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_2])])
 
     def main(self):
         for d in self.data:
@@ -118,6 +107,7 @@ class Client(da.DistProcess):
                 for payload in item['payloads']:
                     req = payload['payload']
                     req['clientId'] = clientId
+                    reqId = req['reqId']
                     type = ''
                     if (req['operation'] == 0):
                         type = 'Query'
@@ -126,47 +116,46 @@ class Client(da.DistProcess):
                     clk = self.logical_clock()
                     self.output(('Sending request to server: ' + str(req['reqId'])))
                     self._send((type, req), self.p)
-                    rclk = None
+                    res = None
 
                     def ExistentialOpExpr_0():
-                        nonlocal rclk
-                        for (_, (rclk, _, _), (_ConstantPattern21_,)) in self._ClientReceivedEvent_0:
-                            if (_ConstantPattern21_ == 'Response'):
-                                if (rclk > clk):
+                        nonlocal res
+                        for (_, _, (_ConstantPattern18_, res)) in self._ClientReceivedEvent_0:
+                            if (_ConstantPattern18_ == 'Response'):
+                                if (res['reqId'] == (reqId - 1)):
                                     return True
                         return False
-                    _st_label_83 = 0
-                    while (_st_label_83 == 0):
-                        _st_label_83 += 1
+                    _st_label_79 = 0
+                    while (_st_label_79 == 0):
+                        _st_label_79 += 1
                         if ExistentialOpExpr_0():
-                            _st_label_83 += 1
+                            _st_label_79 += 1
                         else:
-                            super()._label('_st_label_83', block=True)
-                            _st_label_83 -= 1
+                            super()._label('_st_label_79', block=True)
+                            _st_label_79 -= 1
                     else:
-                        if (_st_label_83 != 2):
+                        if (_st_label_79 != 2):
                             continue
-                    if (_st_label_83 != 2):
+                    if (_st_label_79 != 2):
                         break
-        self._send(('StopServing',), self.p)
 
-    def setup(self, p, nrounds, data):
-        self.nrounds = nrounds
+    def setup(self, p, data):
         self.p = p
         self.data = data
         pass
 
-    def _Client_handler_3(self, res):
+    def _Client_handler_2(self, res):
         self.output(('Received response from server for request: ' + str(res['reqId'])))
         self.output(('Current Balance: ' + str(res['currBal'])))
-    _Client_handler_3._labels = None
-    _Client_handler_3._notlabels = None
+    _Client_handler_2._labels = None
+    _Client_handler_2._notlabels = None
 
 def main():
     da.api.config(clock='Lamport')
     payload = open('/home/ppandey/async/cse535/chain_rep_distalgo/payload.json')
     data = json.load(payload, cls=ConcatJSONDecoder)
-    server = da.api.new(Server, [1, 1], num=1)
-    client = da.api.new(Client, [server, 1, data], num=1)
+    da.api.config(clock='Lamport')
+    server = da.api.new(Server, [1, 4], num=1)
+    client = da.api.new(Client, [server, data], num=1)
     da.api.start(server)
     da.api.start(client)
