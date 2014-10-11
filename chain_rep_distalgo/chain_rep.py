2eac7b602033061f9aaf68e9d7c4829e9ec05ef4
Prashant Pandey <prashant.prashn@gmail.com>
2014-10-10 23:37:50 -0400

    chain replication working for multiple servers and clients. with acknowledgements and everything. check OK.. yipppeeee

--- chain_rep_distalgo/chain_rep.py
+++ chain_rep_distalgo/chain_rep.py
@@ -4,7 +4,11 @@ PatternExpr_0 = da.pat.TuplePattern([da.pat.ConstantPattern('Query'), da.pat.Fre
 PatternExpr_1 = da.pat.FreePattern('p')
 PatternExpr_2 = da.pat.TuplePattern([da.pat.ConstantPattern('Update'), da.pat.FreePattern('req')])
 PatternExpr_3 = da.pat.FreePattern('p')
-PatternExpr_4 = da.pat.TuplePattern([da.pat.ConstantPattern('Response'), da.pat.FreePattern('res')])
+PatternExpr_4 = da.pat.TuplePattern([da.pat.ConstantPattern('Sync'), da.pat.FreePattern('req')])
+PatternExpr_5 = da.pat.FreePattern('p')
+PatternExpr_6 = da.pat.TuplePattern([da.pat.ConstantPattern('Ack'), da.pat.FreePattern('reqId'), da.pat.FreePattern('serverId')])
+PatternExpr_7 = da.pat.FreePattern('p')
+PatternExpr_8 = da.pat.TuplePattern([da.pat.ConstantPattern('Response'), da.pat.FreePattern('res')])
 import sys
 import json
 import pickle
@@ -28,27 +32,33 @@ class Server(da.DistProcess):
 
     def __init__(self, parent, initq, channel, props):
         super().__init__(parent, initq, channel, props)
-        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_0', PatternExpr_0, sources=[PatternExpr_1], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_0]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_1', PatternExpr_2, sources=[PatternExpr_3], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_1])])
+        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_0', PatternExpr_0, sources=[PatternExpr_1], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_0]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_1', PatternExpr_2, sources=[PatternExpr_3], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_1]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_2', PatternExpr_4, sources=[PatternExpr_5], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_2]), da.pat.EventPattern(da.pat.ReceivedEvent, '_ServerReceivedEvent_3', PatternExpr_6, sources=[PatternExpr_7], destinations=None, timestamps=None, record_history=None, handlers=[self._Server_handler_3])])
 
     def main(self):
-        _st_label_22 = 0
-        while (_st_label_22 == 0):
-            _st_label_22 += 1
+        self.output((('ServerId: ' + str(self.serverId)) + ' starting the server operations'))
+        _st_label_27 = 0
+        while (_st_label_27 == 0):
+            _st_label_27 += 1
             if False:
-                _st_label_22 += 1
+                _st_label_27 += 1
             else:
-                super()._label('_st_label_22', block=True)
-                _st_label_22 -= 1
+                super()._label('_st_label_27', block=True)
+                _st_label_27 -= 1
 
-    def setup(self, config, succ, pred):
-        self.config = config
-        self.succ = succ
+    def setup(self, clients, config, pred, succ):
         self.pred = pred
+        self.succ = succ
+        self.config = config
+        self.clients = clients
+        self.serverId = config['serverId']
         self.accDetails = {}
+        self.history = {}
+        self.sentReq = {}
+        self.clientProcessList = list(clients)
 
     def _Server_handler_0(self, p, req):
-        self.output(json.dumps(req))
-        self.output(((('Received request: ' + str(req['reqId'])) + ' from client: ') + str(req['clientId'])))
+        self.output(((('ServerId: ' + str(self.serverId)) + ' ') + json.dumps(req)))
+        self.output(((((('ServerId: ' + str(self.serverId)) + ' Received Query request: ') + str(req['reqId'])) + ' from client: ') + str(req['clientId'])))
         num = req['accNum']
         res = {}
         res['reqId'] = req['reqId']
@@ -64,102 +74,143 @@ class Server(da.DistProcess):
     _Server_handler_0._notlabels = None
 
     def _Server_handler_1(self, p, req):
-        self.output(json.dumps(req))
-        self.output(((('Received request: ' + str(req['reqId'])) + ' from client: ') + str(req['clientId'])))
+        self.output(((('ServerId: ' + str(self.serverId)) + ' ') + json.dumps(req)))
+        self.output(((((('ServerId: ' + str(self.serverId)) + ' Received Update request: ') + str(req['reqId'])) + ' from client: ') + str(req['clientId'])))
         num = req['accNum']
         amt = req['amount']
+        reqId = req['reqId']
         res = {}
-        res['reqId'] = req['reqId']
+        res['reqId'] = reqId
         if (num in self.accDetails):
             bal = self.accDetails[num]
             if (req['operation'] == 1):
                 self.accDetails[num] = (bal + amt)
-                self.output(('Updating the bal: ' + str((bal + num))))
+                self.output(((('ServerId: ' + str(self.serverId)) + ' Updating the bal: ') + str((bal + num))))
                 res['outcome'] = 'Processed'
             elif (req['operation'] == 2):
                 if (bal < amt):
-                    self.output('Not sufficient balance')
+                    self.output((('ServerId: ' + str(self.serverId)) + ' Not sufficient balance'))
                     res['outcome'] = 'InsufficientBalance'
                 else:
                     self.accDetails[num] = (bal - amt)
                     res['outcome'] = 'Processed'
         else:
-            self.output('Account does not exists. Creating new account')
+            self.output((('ServerId: ' + str(self.serverId)) + ' Account does not exists. Creating new account'))
             self.accDetails[num] = amt
             res['outcome'] = 'Processed'
         res['currBal'] = self.accDetails[num]
-        self._send(('Response', res), p)
+        res['payload'] = req
+        self.history[reqId] = req
+        self.sentReq[reqId] = req
+        self._send(('Sync', res), self.succ)
     _Server_handler_1._labels = None
     _Server_handler_1._notlabels = None
+
+    def _Server_handler_2(self, req, p):
+        self.output(((('ServerId: ' + str(self.serverId)) + ' ') + json.dumps(req)))
+        self.output(((('ServerId: ' + str(self.serverId)) + ' Received Sync request: ') + str(req['reqId'])))
+        num = req['payload']['accNum']
+        reqId = req['reqId']
+        clientId = req['payload']['clientId']
+        self.accDetails[num] = req['currBal']
+        self.history[reqId] = req['payload']
+        self.sentReq[reqId] = req['payload']
+        if (self.config['type'] == 2):
+            client = self.clientProcessList[clientId]
+            del req['payload']
+            res = req
+            self._send(('Response', res), client)
+            self._send(('Ack', reqId, self.serverId), self.pred)
+        else:
+            self._send(('Sync', req), self.succ)
+    _Server_handler_2._labels = None
+    _Server_handler_2._notlabels = None
+
+    def _Server_handler_3(self, serverId, reqId, p):
+        self.output(((('ServerId: ' + str(serverId)) + ' ') + str(reqId)))
+        self.output(((('ServerId: ' + str(serverId)) + ' Received Ack request: ') + str(reqId)))
+        for key in range(0, reqId):
+            if (key in self.sentReq):
+                del self.sentReq[key]
+    _Server_handler_3._labels = None
+    _Server_handler_3._notlabels = None
 
 class Client(da.DistProcess):
 
     def __init__(self, parent, initq, channel, props):
         super().__init__(parent, initq, channel, props)
-        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_0', PatternExpr_4, sources=None, destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_2])])
+        self._events.extend([da.pat.EventPattern(da.pat.ReceivedEvent, '_ClientReceivedEvent_0', PatternExpr_8, sources=None, destinations=None, timestamps=None, record_history=None, handlers=[self._Client_handler_4])])
 
     def main(self):
+        self.output((('ClientId: ' + str(self.clientId)) + ' starting the client operations'))
         for d in self.data:
             for item in d['data']:
-                clientId = item['clientId']
-                for payload in item['payloads']:
-                    req = payload['payload']
-                    req['clientId'] = clientId
-                    reqId = req['reqId']
-                    type = ''
-                    if (req['operation'] == 0):
-                        type = 'Query'
-                    else:
-                        type = 'Update'
-                    clk = self.logical_clock()
-                    _st_label_85 = 0
-                    while (_st_label_85 == 0):
-                        _st_label_85 += 1
-                        if (self.lastRecv == (reqId - 1)):
-                            _st_label_85 += 1
+                if (self.clientId == item['clientId']):
+                    for payload in item['payloads']:
+                        req = payload['payload']
+                        req['clientId'] = self.clientId
+                        reqId = req['reqId']
+                        type = ''
+                        if (req['operation'] == 0):
+                            type = 'Query'
                         else:
-                            super()._label('_st_label_85', block=True)
-                            _st_label_85 -= 1
-                    else:
-                        if (_st_label_85 != 2):
-                            continue
-                    if (_st_label_85 != 2):
-                        break
-                    self.output(('Sending request to server: ' + str(req['reqId'])))
-                    self._send((type, req), p)
-        _st_label_88 = 0
-        while (_st_label_88 == 0):
-            _st_label_88 += 1
+                            type = 'Update'
+                        clk = self.logical_clock()
+                        _st_label_119 = 0
+                        while (_st_label_119 == 0):
+                            _st_label_119 += 1
+                            if (self.lastRecv == (reqId - 1)):
+                                _st_label_119 += 1
+                            else:
+                                super()._label('_st_label_119', block=True)
+                                _st_label_119 -= 1
+                        else:
+                            if (_st_label_119 != 2):
+                                continue
+                        if (_st_label_119 != 2):
+                            break
+                        self.output(((('ClientId: ' + str(self.clientId)) + ' Sending request to server: ') + str(req['reqId'])))
+                        idx = self.findServer(req['operation'], req['bankId'])
+                        p = self.serverProcessList[idx]
+                        self._send((type, req), p)
+        _st_label_124 = 0
+        while (_st_label_124 == 0):
+            _st_label_124 += 1
             if False:
-                _st_label_88 += 1
+                _st_label_124 += 1
             else:
-                super()._label('_st_label_88', block=True)
-                _st_label_88 -= 1
+                super()._label('_st_label_124', block=True)
+                _st_label_124 -= 1
 
     def setup(self, servers, config, data):
+        self.data = data
         self.config = config
         self.servers = servers
-        self.data = data
+        self.clientId = config['clientId']
+        self.serverProcessList = list(servers)
         self.lastRecv = 0
-        self.bankServerMap = {}
+        self.responses = []
 
-    def prepareBankServerMap(self):
-        for c in self.config:
-            for bank in c['bank']:
-                id = bank['bankId']
-                server = {}
-                server['headServer'] = bank['headServer']
-                server['tailServer'] = bank['tailServer']
-                self.bankServerMap[id] = server
+    def findServer(self, opr, bankId):
+        if (opr == 0):
+            for bank in self.config['banks']:
+                if (bankId == bank['bankId']):
+                    return bank['tailServer']
+        else:
+            for bank in self.config['banks']:
+                if (bankId == bank['bankId']):
+                    return bank['headServer']
 
-    def _Client_handler_2(self, res):
-        self.output(('Received response from server for request: ' + str(res['reqId'])))
-        self.output(('Current Balance: ' + str(res['currBal'])))
+    def _Client_handler_4(self, res):
+        self.output(((('ClientId: ' + str(self.clientId)) + ' Received response from server for request: ') + str(res['reqId'])))
+        self.output(((('ClientId: ' + str(self.clientId)) + ' Current Balance: ') + str(res['currBal'])))
+        self.responses.append(res)
         self.lastRecv = res['reqId']
-    _Client_handler_2._labels = None
-    _Client_handler_2._notlabels = None
+        self.output(('Responses: ' + json.dumps(self.responses)))
+    _Client_handler_4._labels = None
+    _Client_handler_4._notlabels = None
 
-def loadConfig(config):
+def countProcesses(config):
     count = {}
     servers = 0
     for c in config:
@@ -167,16 +218,17 @@ def loadConfig(config):
             servers += len(bank['servers'])
         count['total_servers'] = servers
         count['total_clients'] = len(c['client'])
-    print(json.dumps(count))
+    print(('Bootstraping: Calculating #  of processes: ' + json.dumps(count)))
     return count
 
 def main():
     da.api.config(clock='Lamport')
+    print('Bootstraping: loading and parsing the config file')
     dataFile = open('/home/ppandey/async/cse535/chain_rep_distalgo/payload.json')
     data = json.load(dataFile, cls=ConcatJSONDecoder)
     cfgFile = open('/home/ppandey/async/cse535/chain_rep_distalgo/config.json')
     config = json.load(cfgFile, cls=ConcatJSONDecoder)
-    count = loadConfig(config)
+    count = countProcesses(config)
     servers = da.api.new(Server, num=count['total_servers'])
     clients = da.api.new(Client, num=count['total_clients'])
     clientMap = []
@@ -192,16 +244,20 @@ def main():
                 conf['type'] = s['type']
                 conf['serverId'] = s['serverId']
                 serverMap.append(conf)
+    print('Bootstraping: Setting up client/server processes')
     i = 0
     serList = list(servers)
     for (process, config) in zip(serList, serverMap):
         if (config['type'] == 0):
-            da.api.setup({process}, (config, None, serList[(i + 1)]))
+            da.api.setup({process}, (clients, config, None, serList[(i + 1)]))
         elif (config['type'] == 2):
-            da.api.setup({process}, (config, serList[(i - 1)], None))
+            da.api.setup({process}, (clients, config, serList[(i - 1)], None))
         else:
-            da.api.setup({process}, (config, serList[(i - 1)], serList[(i + 1)]))
+            da.api.setup({process}, (clients, config, serList[(i - 1)], serList[(i + 1)]))
         i += 1
     cltList = list(clients)
     for (process, config) in zip(cltList, clientMap):
         da.api.setup({process}, (servers, config, data))
+    print('Bootstraping: Starting client/server processes')
+    da.api.start(servers)
+    da.api.start(clients)
